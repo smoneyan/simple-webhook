c3aa6cc311ada89110facaf4e7be45a18d10ed9a
Subramanian Swaminathan <837733+smoneyan@users.noreply.github.com>
2022-02-02 12:22:01 +0800

    adding dockerignore

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const app = express();
 const PORT = 3000;
 // Tell express to use body-parser's JSON parsing
 app.use(bodyParser.text({ type: "text/plain" }));
-app.use(bodyParser.json({ type: "application/*" }));
+app.use(bodyParser.urlencoded({ type: "application/x-www-form-urlencoded" }));
+// app.use(bodyParser.text({ type: "*" }));
+app.use(bodyParser.json({ type: "application/json" }));
 // Start express on the defined port
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
@@ -15,33 +17,39 @@ app.post("/webhook", (req, res) => {
   let response;
   if ("application/json" === req.headers["content-type"]) {
     response = req.body;
+    console.log(response);
+    res.json(response);
+
+    if (response.hasOwnProperty("SubscribeURL")) {
+      url = response.SubscribeURL;
+      console.log(url);
+      if (typeof url !== "undefined" && url !== null) {
+        https
+          .get(url, (resp) => {
+            let data = "";
+
+            // A chunk of data has been received.
+            resp.on("data", (chunk) => {
+              data += chunk;
+            });
+
+            // The whole response has been received. Print out the result.
+            resp.on("end", () => {
+              console.log(data);
+            });
+          })
+          .on("error", (err) => {
+            console.log("Error: " + err.message);
+          });
+      }
+    }
   } else {
-    response = JSON.parse(req.body);
-  }
-  console.log(response);
-  res.json(response);
-
-  if (response.hasOwnProperty("SubscribeURL")) {
-    url = response.SubscribeURL;
-    console.log(url);
-    if (typeof url !== "undefined" && url !== null) {
-      https
-        .get(url, (resp) => {
-          let data = "";
-
-          // A chunk of data has been received.
-          resp.on("data", (chunk) => {
-            data += chunk;
-          });
-
-          // The whole response has been received. Print out the result.
-          resp.on("end", () => {
-            console.log(data);
-          });
-        })
-        .on("error", (err) => {
-          console.log("Error: " + err.message);
-        });
+    try {
+      response = JSON.parse(req.body);
+    } catch (e) {
+      console.log("Not a valid JSON sent");
+      res.status(200).end(); // Responding is important
+      return false;
     }
   }
 
